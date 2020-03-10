c49f5567226cada8c8d43ccb6e98477b6219ab24
Talila Burka <btalila@yahoo.com>
2020-03-09 20:12:20 -0500

    Added notes.html, made changes to index.js

--- server/index.js
+++ server/index.js
@@ -4,9 +4,14 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
+app.get('/', (_, res) => {
     const filePath = path.resolve(__dirname, '..', 'public', 'index.html');
-    res.sendFile(filePath);
+    res.sendFile(filePath)
+});
+
+app.get('/notes', (_, res) => {
+    const filePath = path.resolve(__dirname, '..', 'public', 'notes.html');
+    res.sendFile(filePath)
 });
 
 app.listen(port, () =>{ 
