758501bd2a85ade3305d1947da557a80aa924c11
Talila Burka <btalila@yahoo.com>
2020-03-12 00:19:02 -0500

    added database and updated assets w javascript

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -5,3 +5,100 @@ const $newNoteBtn = $(".new-note");
 const $noteList = $(".list-container .list-group");
 
 let activeNote = {};
+
+const getNotes = function() {
+    return $.ajax({
+      url: "/api/notes",
+      method: "GET"
+    });
+  };
+
+  const saveNote = function(note) {
+    return $.ajax({
+      url: "/api/notes",
+      data: note,
+      method: "POST"
+    });
+  };
+
+  const deleteNote = function(id) {
+    return $.ajax({
+      url: "/api/notes/" + id,
+      method: "DELETE"
+    });
+  };
+
+  const renderActiveNote = function() {
+    $saveNoteBtn.hide();
+  
+    if (activeNote.id) {
+      $noteTitle.attr("readonly", true);
+      $noteText.attr("readonly", true);
+      $noteTitle.val(activeNote.title);
+      $noteText.val(activeNote.text);
+    } else {
+      $noteTitle.attr("readonly", false);
+      $noteText.attr("readonly", false);
+      $noteTitle.val("");
+      $noteText.val("");
+    }
+  };
+
+  const handleNoteSave = function() {
+    const newNote = {
+      title: $noteTitle.val(),
+      text: $noteText.val()
+    };
+    saveNote(newNote).then(function(_) {
+        getAndRenderNotes();
+        renderActiveNote();
+      });
+    };
+    const handleNoteView = function() {
+        activeNote = $(this).data();
+        renderActiveNote();
+      };
+      const handleNewNoteView = function() {
+        activeNote = {};
+        renderActiveNote();
+      };
+
+      const handleRenderSaveBtn = function() {
+        if (!$noteTitle.val().trim() || !$noteText.val().trim()) {
+          $saveNoteBtn.hide();
+        } else {
+          $saveNoteBtn.show();
+        }
+      };
+      const renderNoteList = function(notes) {
+        $noteList.empty();
+      
+        const noteListItems = [];
+      
+        for (const note of notes) {
+          const $li = $("<li class='list-group-item'>").data(note);
+          const $span = $("<span>").text(note.title);
+          const $delBtn = $(
+            "<i class='fas fa-trash-alt float-right text-danger delete-note'>"
+          );
+      
+          $li.append($span, $delBtn);
+          noteListItems.push($li);
+        }
+      
+        $noteList.append(noteListItems);
+      };
+      const getAndRenderNotes = function() {
+        return getNotes().then(function(data) {
+          renderNoteList(data);
+        });
+      };
+      
+      $saveNoteBtn.on("click", handleNoteSave);
+      $noteList.on("click", ".list-group-item", handleNoteView);
+      $newNoteBtn.on("click", handleNewNoteView);
+      $noteList.on("click", ".delete-note", handleNoteDelete);
+      $noteTitle.on("keyup", handleRenderSaveBtn);
+      $noteText.on("keyup", handleRenderSaveBtn);
+
+      getAndRenderNotes();
